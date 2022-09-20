d066d08461c0267d68fd2b7ff92da11b46234523
Arun chandra roy <48654733+codersurjo@users.noreply.github.com>
2022-09-20 07:25:58 +0600

    color add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const color ={
+  color:'red',
+  bacground: '#FFF',
+  borerRadius:'10px',
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 style={color}>JSX Design</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
